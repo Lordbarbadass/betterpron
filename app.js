4df7d46f8a8d66b1d86ff3074cee8b045efeb198
terrygonguet <terry@gonguet.com>
2018-07-14 21:17:17 +0200

    moved to git

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs')
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/gallery", async (req, res) => {
+  try {
+    let cache = JSON.parse(fs.readFileSync("cache.json"));
+    if (cache[req.query.url]) {
+      return res.json({ error:false, images:cache[req.query.url] });
+    }
+
+    let gallery = await rp.get(req.query.url, { qs: { nw: "session" }, jar: true });
+    let $ = cheerio.load(gallery);
+    let first = $(".gdtm a:first-child").attr("href");
+    let images = await getImages(first);
+    res.json({ error:false, images });
+
+    cache[req.query.url] = images;
+    fs.writeFile("cache.json", JSON.stringify(cache, null, 2), () => console.log(`Cached ${req.query.url}`));
+  } catch (error) {
+    console.error(error);
+   res.json({ error:true, message:error }) 
+  }
+});
+
+async function getImages (url, acc=[]) {
+  console.log(`Getting ${url}`);
+  
+  let page = await rp.get(url);
+  let $ = cheerio.load(page);
+  let a = $("#i3 a");
+  let nextUrl = a.attr("href");
+  let img = a.find("img").attr("src");
+  acc.push(img);
+  if (url !== nextUrl) await getImages(nextUrl, acc);
+  return acc;
+}
+
+app.listen(process.env.PORT || 3000, () => console.log("Listening on port " + (process.env.PORT || 3000)));
---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+let app = new Vue({
+  el: "#container",
+  data: {
+    url: "",
+    images: [],
+    urlRegExp: /https:\/\/e-hentai.org\/g\/(?:.+?)\/(?:.+?)\//,
+    isLoading: false,
+    page: 0
+  },
+  methods: {
+    async getGallery() {
+      this.isLoading = true;
+      this.page = 0;
+      let res = await fetch(`/gallery?url=${encodeURI(this.url)}`, { headers: { "Content-Type": "application/json" } });
+      this.isLoading = false;
+      data = await res.json();
+      if (!data.error)
+        this.images = data.images;
+      else
+        console.error(data.message);
+    },
+    movePage (delta) {
+      this.page += delta;
+      if (this.page < 0) this.page = 0;
+      window.scrollTo(0,0);
+    }
+  },
+  computed: {
+    isUrlValid() {
+      return this.url && this.urlRegExp.test(this.url);
+    },
+    curPage() {
+      return this.images ? this.images.slice(this.page * 50, this.page * 50 + 50) : [];
+    }
+  },
+  watch: {
+    url (val, oldval) {
+      if (!this.isUrlValid) return;
+      else this.getGallery();
+    }
+  }
+})
