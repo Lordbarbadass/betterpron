4b69eec85dcf6b8c56f3abd9814e0773840ad1a1
terrygonguet <terry@gonguet.com>
2018-07-22 00:57:17 +0200

    Heroku troubleshooting

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.get("/gallery", async (req, res) => {
     // }
 
     let gallery = await rp.get(req.query.url, { qs: { nw: "session" }, jar: true });
+    console.log(gallery);
+
     let $ = cheerio.load(gallery);
     let first = $(".gdtm a:first-child").attr("href");
     let images = await getImages(first);
@@ -32,7 +34,7 @@ app.get("/gallery", async (req, res) => {
     // fs.writeFile("cache.json", JSON.stringify(cache, null, 2), () => console.log(`Cached ${req.query.url}`));
   } catch (error) {
     console.error(error);
-   res.json({ error:true, message:error }) 
+    res.json({ error:true, message:error }) 
   }
 });
 
